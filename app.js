62dd05f9301702752404b13c72e24b2edbae0362
Hayley Gerard <gerard@sonoma.edu>
2016-12-14 16:19:15 -0800

    Everything for Lab 10 works

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var school = require('./routes/school_routes');
 
+var address = require('./routes/address_routes');
+var account = require('./routes/account_routes');
+var resume = require('./routes/resume_routes');
+var company = require('./routes/company_routes');
+var skill = require('./routes/skill_routes');
+
 var app = express();
 
 // view engine setup
@@ -25,6 +31,21 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/school', school);
+
+app.use('/', index);
+app.use('/address', address);
+
+app.use('/', index);
+app.use('/account', account);
+
+app.use('/', index);
+app.use('/resume', resume);
+
+app.use('/', index);
+app.use('/company', company);
+
+app.use('/', index);
+app.use('/skill', skill);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
